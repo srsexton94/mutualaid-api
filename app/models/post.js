3b8f755a1fa54543164a5f8f64c0798f4af50c9d
srsexton94 <srsexton94@gmail.com>
2020-03-30 10:26:09 -0400

    Add 'label' attribute to post model
    Closes #7

--- app/models/post.js
+++ app/models/post.js
@@ -9,6 +9,10 @@ const postSchema = new mongoose.Schema({
     type: String,
     required: true
   },
+  label: {
+    type: String,
+    required: true
+  },
   zip: {
     type: Number,
     required: true
